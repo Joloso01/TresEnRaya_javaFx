d1402aeb963f8f3fbb4f93bdd6c14a7168d659af
Joloso01 <jbermejop01@elpuig.xeill.net>
2021-02-08 13:43:22 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(new Scene(new BorderPane()));
+        primaryStage.show();
+    }
+}
