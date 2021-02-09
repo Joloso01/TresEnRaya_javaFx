a91276eb4e2837e50f0d9eca3cfeaffbc0c93320
jose <jbermejop01@elpuig.xeill.net>
2021-02-09 17:54:20 +0100

    Acción Boton añadida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
+import controller.MainWindow;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainWindow.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        MainWindow mainWindow = loader.getController();
+        mainWindow.setStage(primaryStage);
+        mainWindow.setScene(scene);
+
+        primaryStage.setTitle("Tres en raya");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setScene(new Scene(new BorderPane()));
-        primaryStage.show();
+    public static void main(String[] args){
+        launch(args);
     }
 }
--- src/main/java/controller/MainWindow.java
+++ src/main/java/controller/MainWindow.java
@@ -1,4 +1,39 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MainWindow {
+
+    private Scene scene;
+    private Stage stage;
+    int turno=1;
+
+    @FXML
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+
+    public void boton(ActionEvent actionEvent) {
+        Button b = (Button) actionEvent.getSource();
+
+        if (turno %2 == 0){
+            b.setText("O");
+            turno++;
+        }else {
+            b.setText("X");
+            turno++;
+        }
+    }
 }
